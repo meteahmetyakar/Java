53fea4c8ab26c8d89ae8f6e538c37a1d81d17838
Mete Ahmet Yakar <meteahmetyakar@gmail.com>
2023-09-21 22:07:33 +0300

    added description

--- UDPClient/src/Main.java
+++ UDPClient/src/Main.java
@@ -2,14 +2,11 @@ import java.io.IOException;
 import java.net.*;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
 	public static void main(String[] args) {
 
-		try {
+		try(DatagramSocket datagramSocket = new DatagramSocket();){
 			InetAddress address = InetAddress.getLocalHost();
-			DatagramSocket datagramSocket = new DatagramSocket();
 
 			Scanner scanner = new Scanner(System.in);
 			String echoString;
