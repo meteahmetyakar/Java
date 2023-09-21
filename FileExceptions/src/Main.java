142038b77f699b92eb2c14079cb8da8c57a1b906
Mete Ahmet Yakar <meteahmetyakar@gmail.com>
2023-09-21 06:46:07 +0300

    added topics

--- FileExceptions/src/Main.java
+++ FileExceptions/src/Main.java
@@ -9,8 +9,7 @@ import java.nio.file.Paths;
 import java.rmi.server.ExportException;
 import java.util.List;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
 	public static void main(String[] args) {
 
